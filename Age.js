e9c914668efad1057fd3c5daa353c7527d9f03ec
Ashfaqrokhan <ashfaqrokhan@gmail.com>
2024-08-07 11:54:00 +0500

    add file for student data

---
+++ Age.js
@@ -0,0 +1,4 @@
+var firstName = prompt("Enter your first name");
+var lastName = prompt("Enter your last name");
+var age = prompt("Enter your age");
+alert("Make sure your given data is correct:  \n "  +" firstName:" + firstName + "\n " +" lastName:" + lastName + "\n  age: "  + age);
